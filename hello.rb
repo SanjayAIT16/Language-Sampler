d922a862c5504c5c37a9307740f1e414e2d74828
SanjayAIT16 <153589336+SanjayAIT16@users.noreply.github.com>
2024-02-20 20:02:11 +0530

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -6,3 +6,18 @@ puts "Hello, world!"
 puts "Hello"
 puts "Hello, world!"
 puts "Hello"
+
+def odd_or_even(number)
+  if number % 2 == 0
+    puts "#{number} is even."
+  else
+    puts "#{number} is odd."
+  end
+end
+
+# Test the function with some numbers
+odd_or_even(5)
+odd_or_even(10)
+odd_or_even(23)
+odd_or_even(100)
+
